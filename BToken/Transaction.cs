39a4815e17d740d2b4a9924e1e988be859c4ae68
GalackQSM <157456816+GalackduQSM@users.noreply.github.com>
2025-07-25 11:01:47 -0700

    Transaction.cs

---
+++ BToken/Transaction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinSwapBot.Domain.Models
+{
+    public enum TransactionType
+    {
+        Purchase,
+        Exchange
+    }
+
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int CurrencyId { get; set; }
+        public TransactionType Type { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+
+
+        public Client Client { get; set; }
+        public Currency Currency { get; set; }
+    }
+}
