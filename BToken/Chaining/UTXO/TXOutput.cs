9b2fc800afeb730978159a929cebf2f34d36cb1c
GalackQSM <157456816+GalackduQSM@users.noreply.github.com>
2025-07-25 11:02:37 -0700

    TXOutput.cs

---
+++ BToken/Chaining/UTXO/TXOutput.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BToken.Chaining
+{
+  partial class UTXOTable
+  {
+    public struct TXOutput
+    {
+      public ulong Value;
+
+      public byte[] Buffer;
+      public int StartIndexScript;
+      public int LengthScript;
+
+
+      public TXOutput(
+        byte[] buffer,
+        ref int index)
+      {
+        Value = BitConverter.ToUInt64(
+          buffer,
+          index);
+
+        index += 8;
+
+        Buffer = buffer;
+
+        LengthScript = VarInt.GetInt32(
+          Buffer,
+          ref index);
+
+        StartIndexScript = index;
+        index += LengthScript;
+      }
+    }
+  }
+}
