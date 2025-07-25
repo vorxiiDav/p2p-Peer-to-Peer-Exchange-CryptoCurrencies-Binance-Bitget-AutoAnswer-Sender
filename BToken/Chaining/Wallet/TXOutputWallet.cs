688d18f97efcf8bb3965e98d683aee2493c6da0c
GalackQSM <157456816+GalackduQSM@users.noreply.github.com>
2025-07-25 11:02:52 -0700

    TXOutputWallet.cs

---
+++ BToken/Chaining/Wallet/TXOutputWallet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BToken.Chaining
+{
+  partial class UTXOTable
+  {
+    partial class WalletUTXO
+    {
+      class TXOutputWallet
+      {
+        public byte[] TXID = new byte[HASH_BYTE_SIZE];
+        public int TXIDShort;
+        public int OutputIndex;
+        public ulong Value;
+        public byte[] ScriptPubKey = new byte[LENGTH_P2PKH];
+      }
+    }
+  }
+}
