9dfd275ebae5ab3388e9da3a47d883d906f209cc
GalackQSM <157456816+GalackduQSM@users.noreply.github.com>
2025-07-25 11:02:08 -0700

    AddressMessage.cs

---
+++ BToken/Chaining/Messages/AddressMessage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BToken.Chaining
+{
+  class AddressMessage : NetworkMessage
+  {
+    public List<NetworkAddress> NetworkAddresses = 
+      new List<NetworkAddress>();
+
+
+    public AddressMessage(byte[] messagePayload) 
+      : base("addr", messagePayload)
+    {
+      int startIndex = 0;
+
+      int addressesCount = VarInt.GetInt32(
+        Payload, 
+        ref startIndex);
+
+      for (int i = 0; i < addressesCount; i++)
+      {
+        NetworkAddresses.Add(
+          NetworkAddress.ParseAddress(
+            Payload, ref startIndex));
+      }
+    }
+  }
+}
