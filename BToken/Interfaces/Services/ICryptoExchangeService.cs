eead86c9e16cae0164ab2bfd5c154ad730982055
GalackQSM <157456816+GalackduQSM@users.noreply.github.com>
2025-07-25 11:03:33 -0700

    ICryptoExchangeService.cs

---
+++ BToken/Interfaces/Services/ICryptoExchangeService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinSwapBot.Domain.Interfaces.Services
+{
+    public interface ICryptoExchangeService
+    {
+        Task<Dictionary<string, decimal>> GetBalance(string publicKey, string privateKey);
+    }
+}
