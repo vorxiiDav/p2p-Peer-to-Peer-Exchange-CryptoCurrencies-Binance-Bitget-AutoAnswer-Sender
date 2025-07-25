4418fcd254e4613d63ef71cded1193737bd0cda7
GalackQSM <157456816+GalackduQSM@users.noreply.github.com>
2025-07-25 11:02:18 -0700

    NetworkMessage.cs

---
+++ BToken/Chaining/Messages/NetworkMessage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BToken.Chaining
+{
+  public class NetworkMessage
+  {
+    public string Command;
+    public byte[] Payload;
+
+
+
+    public NetworkMessage(string command)
+      : this(command, new byte[0])
+    { }
+
+    public NetworkMessage(
+      string command, 
+      byte[] payload)
+    {
+      Command = command;
+      Payload = payload;
+    }
+  }
+}
