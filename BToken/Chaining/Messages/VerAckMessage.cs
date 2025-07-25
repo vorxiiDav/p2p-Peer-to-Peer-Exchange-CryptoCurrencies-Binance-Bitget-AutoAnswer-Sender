c1ac26672ba7d3fa451a48fd9754f228f9eb71a7
GalackQSM <157456816+GalackduQSM@users.noreply.github.com>
2025-07-25 11:02:27 -0700

    VerAckMessage.cs

---
+++ BToken/Chaining/Messages/VerAckMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BToken.Chaining
+{
+  class VerAckMessage : NetworkMessage
+  {
+
+
+    public VerAckMessage() : base("verack")
+    {
+    }
+  }
+}
