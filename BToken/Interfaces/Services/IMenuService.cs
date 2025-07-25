7f59904769bcb934cef07ccd5276c57ea3e89eeb
GalackQSM <157456816+GalackduQSM@users.noreply.github.com>
2025-07-25 11:03:40 -0700

    IMenuService.cs

---
+++ BToken/Interfaces/Services/IMenuService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+namespace CoinSwapBot.Domain.Interfaces.Services
+{
+    public interface IMenuService
+    {
+        Task HandleUpdate(Update update);
+    }
+}
