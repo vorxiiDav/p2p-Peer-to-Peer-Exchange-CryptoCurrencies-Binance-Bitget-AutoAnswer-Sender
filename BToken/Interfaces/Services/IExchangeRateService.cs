89bf1234e834e3a0b2cfcae5bc5e85b915b0daec
GalackQSM <157456816+GalackduQSM@users.noreply.github.com>
2025-07-25 11:03:35 -0700

    IExchangeRateService.cs

---
+++ BToken/Interfaces/Services/IExchangeRateService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinSwapBot.Domain.Interfaces.Services
+{
+    public interface IExchangeRateService
+    {
+        Task<Dictionary<string, decimal>> GetExchangeRate(string currency);
+    }
+}
