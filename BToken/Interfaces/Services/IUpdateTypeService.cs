b589903e65bfcc8bd8d3f08c3f415957829d1c0d
GalackQSM <157456816+GalackduQSM@users.noreply.github.com>
2025-07-25 11:03:42 -0700

    IUpdateTypeService.cs

---
+++ BToken/Interfaces/Services/IUpdateTypeService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+namespace CoinSwapBot.Domain.Interfaces.Services
+{
+    public interface IUpdateTypeService
+    {
+        bool IsTextMessage(Update update);
+        bool IsCallbackQuery(Update update);
+    }
+}
