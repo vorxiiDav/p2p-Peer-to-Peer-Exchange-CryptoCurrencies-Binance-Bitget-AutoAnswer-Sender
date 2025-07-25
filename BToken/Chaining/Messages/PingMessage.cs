14a7354e09414cc05a314d765b487803415b41af
GalackQSM <157456816+GalackduQSM@users.noreply.github.com>
2025-07-25 11:02:20 -0700

    PingMessage.cs

---
+++ BToken/Chaining/Messages/PingMessage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BToken.Chaining
+{
+  class PingMessage : NetworkMessage
+  {
+    public UInt64 Nonce { get; private set; }
+
+
+    public PingMessage(NetworkMessage networkMessage) 
+      : base("ping", networkMessage.Payload)
+    {
+      Nonce = BitConverter.ToUInt64(Payload, 0);
+    }
+    public PingMessage(UInt64 nonce) : base("ping")
+    {
+      Nonce = nonce;
+      Payload = BitConverter.GetBytes(nonce);
+    }
+  }
+}
