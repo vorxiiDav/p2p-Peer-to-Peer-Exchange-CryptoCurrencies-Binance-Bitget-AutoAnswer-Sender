723a3143baf4a3d0b4f132b7d0cebee47b45611d
GalackQSM <157456816+GalackduQSM@users.noreply.github.com>
2025-07-25 11:02:36 -0700

    TXInput.cs

---
+++ BToken/Chaining/UTXO/TXInput.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BToken.Chaining
+{
+  partial class UTXOTable
+  {
+    public struct TXInput
+    {
+      public int StartIndexScript;
+      public int LengthScript;
+
+      public byte[] TXIDOutput;
+      public int TXIDOutputShort;
+      public int OutputIndex;
+
+
+
+      //public TXInput(
+      //  byte[] tXIDOutput,
+      //  int outputIndex)
+      //{
+      //  TXIDOutput = tXIDOutput;
+      //  PrimaryKeyTXIDOutput = BitConverter.ToInt32(tXIDOutput, 0);
+      //  OutputIndex = outputIndex;
+      //}
+
+
+      public TXInput(byte[] buffer, ref int index)
+      {
+        TXIDOutput = new byte[HASH_BYTE_SIZE];
+
+        Array.Copy(
+          buffer, 
+          index, 
+          TXIDOutput, 
+          0, 
+          HASH_BYTE_SIZE);
+
+        TXIDOutputShort = BitConverter.ToInt32(
+          buffer,
+          index);
+
+        index += HASH_BYTE_SIZE;
+
+        OutputIndex = BitConverter.ToInt32(
+          buffer, 
+          index);
+
+        index += 4;
+
+        LengthScript = VarInt.GetInt32(
+          buffer, 
+          ref index);
+
+        StartIndexScript = index;
+
+        index += LengthScript;
+
+        index += 4; // sequence
+      }
+    }
+  }
+}
