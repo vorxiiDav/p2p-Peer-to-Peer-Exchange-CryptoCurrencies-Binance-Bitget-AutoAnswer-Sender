b54d8dbd5b1f2c4a778ce9aad94fcc88aed7d82c
GalackQSM <157456816+GalackduQSM@users.noreply.github.com>
2025-07-25 11:01:45 -0700

    Program.cs

---
+++ BToken/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BToken
+{
+  static class Program
+  {
+    public static Node Node;
+
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+      try
+      {
+        Node = new Node();
+        Node.Start();
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.Message);
+      }
+
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new Form1());
+    }
+  }
+}
