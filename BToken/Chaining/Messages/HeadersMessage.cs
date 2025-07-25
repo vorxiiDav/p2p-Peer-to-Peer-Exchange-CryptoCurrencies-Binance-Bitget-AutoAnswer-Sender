55bcd069d651cbd83a21eb8911ace4a355f1ddfd
GalackQSM <157456816+GalackduQSM@users.noreply.github.com>
2025-07-25 11:02:15 -0700

    HeadersMessage.cs

---
+++ BToken/Chaining/Messages/HeadersMessage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+
+namespace BToken.Chaining
+{
+  class HeadersMessage : NetworkMessage
+  {
+    public List<Header> Headers = new List<Header>();
+
+
+    public HeadersMessage(
+      List<Header> headers)
+      : base("headers")
+    {
+      Headers = headers;
+      SerializePayload();
+    }
+    void SerializePayload()
+    {
+      var payload = new List<byte>();
+
+      payload.AddRange(VarInt.GetBytes(Headers.Count));
+
+      foreach (Header header in Headers)
+      {
+        payload.AddRange(header.GetBytes());
+        payload.Add(0);
+      }
+
+      Payload = payload.ToArray();
+    }
+  }
+}
