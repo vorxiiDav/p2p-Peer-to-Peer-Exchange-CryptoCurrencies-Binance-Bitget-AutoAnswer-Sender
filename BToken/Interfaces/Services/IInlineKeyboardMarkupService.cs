20fa140f965528597767e4fc2f9de2d76ac2f94c
GalackQSM <157456816+GalackduQSM@users.noreply.github.com>
2025-07-25 11:03:37 -0700

    IInlineKeyboardMarkupService.cs

---
+++ BToken/Interfaces/Services/IInlineKeyboardMarkupService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace CoinSwapBot.Domain.Interfaces.Services
+{
+    public interface IInlineKeyboardMarkupService
+    {
+        InlineKeyboardMarkup CreateStartMenuInlineKeyboard();
+        InlineKeyboardMarkup CreateBackAndMenuInlineKeyboard();
+        InlineKeyboardMarkup CreateCalculateAndBackInlineKeyboard();
+        InlineKeyboardMarkup CreateCryptocurrencyAndMenuInlineKeyboard();
+        InlineKeyboardMarkup CreateContactsAndMenuInlineKeyboard();
+        InlineKeyboardMarkup CreateTelegramAndMenuButtonInlineKeyboard();
+        InlineKeyboardMarkup CreateLanguagesAndMenuButtonInlineKeyboard();
+    }
+}
