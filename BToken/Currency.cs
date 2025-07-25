8c19b6db5d0d1f4cf8b7d16234dd4923ed295121
GalackQSM <157456816+GalackduQSM@users.noreply.github.com>
2025-07-25 11:01:33 -0700

    Currency.cs

---
+++ BToken/Currency.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinSwapBot.Domain.Models
+{
+    public enum CryptoCurrency
+    {
+        ETH,
+        BTC
+    }
+
+    public class Currency
+    {
+        public int Id { get; set; }
+        public CryptoCurrency Name { get; set; }
+        public decimal CurrentRate { get; set; }
+
+        public List<Transaction> Transactions { get; set; }
+    }
+}
