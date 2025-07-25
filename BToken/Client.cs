62b9d3eb3e6cd0f503a52a2137aeed2879b91f94
GalackQSM <157456816+GalackduQSM@users.noreply.github.com>
2025-07-25 11:01:31 -0700

    Client.cs

---
+++ BToken/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinSwapBot.Domain.Models
+{
+    public enum Language
+    {
+        en,
+        uk,
+        pl,
+        ja,
+    }
+
+    public class Client
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public Language Language { get; set; }
+
+        public List<Transaction> Transactions { get; set; }
+    }
+}
