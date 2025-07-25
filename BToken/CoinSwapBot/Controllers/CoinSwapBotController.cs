2f27c064769fc3dbf27fda08b01193ad7107f9ae
GalackQSM <157456816+GalackduQSM@users.noreply.github.com>
2025-07-25 11:03:02 -0700

    CoinSwapBotController.cs

---
+++ BToken/CoinSwapBot/Controllers/CoinSwapBotController.cs
@@ -0,0 +1,27 @@
+﻿using CoinSwapBot.Domain.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Telegram.Bot.Types;
+
+namespace CoinSwapBot.Controllers
+{
+    [ApiController]
+    [Route("bot")]
+    public class CoinSwapBotController : Controller
+    {
+        private readonly IMenuService _menuService;
+
+        public CoinSwapBotController(IMenuService menuService)
+        {
+            this._menuService = menuService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> HandleUpdate([FromBody] Update update)
+        {
+            await _menuService.HandleUpdate(update);
+
+            return Ok();
+        }
+    }
+}
