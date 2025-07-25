ce99e08553594a7096ab572f165c3fe31e5d87d0
GalackQSM <157456816+GalackduQSM@users.noreply.github.com>
2025-07-25 11:03:38 -0700

    ILocalizationService.cs

---
+++ BToken/Interfaces/Services/ILocalizationService.cs
@@ -0,0 +1,15 @@
+﻿using CoinSwapBot.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinSwapBot.Domain.Interfaces.Services
+{
+    public interface ILocalizationService
+    {
+        Task<string> GetCurrentLanguage(string username);
+        Task SetLanguage(string username, string languageCode);
+    }
+}
