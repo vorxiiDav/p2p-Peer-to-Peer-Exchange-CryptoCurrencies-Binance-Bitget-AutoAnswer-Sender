36eef77f5ed18b0fdd37e197e50a80b1d4ca4df1
GalackQSM <157456816+GalackduQSM@users.noreply.github.com>
2025-07-25 11:02:22 -0700

    PongMessage.cs

---
+++ BToken/Chaining/Messages/PongMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BToken.Chaining
+{
+  class PongMessage : NetworkMessage
+  {
+    public PongMessage(UInt64 nonce) : base("pong")
+    {
+      Payload = BitConverter.GetBytes(nonce);
+    }
+  }
+}
