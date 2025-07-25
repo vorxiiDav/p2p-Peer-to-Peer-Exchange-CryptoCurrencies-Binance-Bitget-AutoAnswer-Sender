9f85686593789922bee3b53619759492484b0a70
GalackQSM <157456816+GalackduQSM@users.noreply.github.com>
2025-07-25 11:03:31 -0700

    IClientRepository.cs

---
+++ BToken/Interfaces/Repositories/IClientRepository.cs
@@ -0,0 +1,18 @@
+﻿using CoinSwapBot.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinSwapBot.Domain.Interfaces.Repositories
+{
+    public interface IClientRepository
+    {
+        Task CreateAsync(Client client);
+        Task<Client> FindById(int clientId);
+        Task SaveAsync();
+        Task UpdateAsync(Client client);
+        Task<Client> GetByUsername(string username);
+    }
+}
