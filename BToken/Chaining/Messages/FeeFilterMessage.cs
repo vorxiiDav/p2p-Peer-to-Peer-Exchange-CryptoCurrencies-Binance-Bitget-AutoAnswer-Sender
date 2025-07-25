c7c06565a43d3ca9e9849ccb8a1ad65ee2b41374
GalackQSM <157456816+GalackduQSM@users.noreply.github.com>
2025-07-25 11:02:10 -0700

    FeeFilterMessage.cs

---
+++ BToken/Chaining/Messages/FeeFilterMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BToken.Chaining
+{
+  class FeeFilterMessage : NetworkMessage
+  {
+    public ulong FeeFilterValue { get; private set; }
+
+    public FeeFilterMessage(byte[] messagePayload) 
+      : base("feefilter", messagePayload)
+    { }
+  }
+}
