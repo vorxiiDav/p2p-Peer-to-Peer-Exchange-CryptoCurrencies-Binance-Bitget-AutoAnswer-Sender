9bc5963a7c067dd9ac28a40109526d177cfaa58d
GalackQSM <157456816+GalackduQSM@users.noreply.github.com>
2025-07-25 11:01:54 -0700

    ProtocolException.cs

---
+++ BToken/Chaining/ProtocolException.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BToken.Chaining
+{
+  enum ErrorCode {
+    DUPLICATE,
+    ORPHAN,
+    INVALID };
+
+  class ProtocolException : Exception
+  {
+    public ErrorCode ErrorCode;
+    
+
+    public ProtocolException()
+    { }
+
+    public ProtocolException(string message)
+        : base(message)
+    { }
+
+    public ProtocolException(
+      string message, 
+      ErrorCode errorCode)
+        : base(message)
+    {
+      ErrorCode = errorCode;
+    }
+
+    public ProtocolException(string message, Exception inner)
+        : base(message, inner)
+    { }
+
+  }
+}
