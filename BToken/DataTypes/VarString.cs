c4e7e9a88e4d971e1cc5976ab3b1fce1b3a7ea31
GalackQSM <157456816+GalackduQSM@users.noreply.github.com>
2025-07-25 11:03:12 -0700

    VarString.cs

---
+++ BToken/DataTypes/VarString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BToken
+{
+  public static class VarString
+  {
+    public static string GetString(byte[] buffer, ref int startIndex)
+    {
+      int stringLength = VarInt.GetInt32(buffer, ref startIndex);
+      string text = Encoding.ASCII.GetString(buffer, startIndex, stringLength);
+
+      startIndex += stringLength;
+      return text;
+    }
+
+
+    public static List<byte> GetBytes(string text)
+    {
+      List<byte> serializedValue = VarInt.GetBytes(text.Length);
+      serializedValue.AddRange(Encoding.ASCII.GetBytes(text));
+
+      return serializedValue;
+    }
+  }
+}
