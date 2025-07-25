999a74810f4d75ba5835e9a8e694b48d6ef6d31f
GalackQSM <157456816+GalackduQSM@users.noreply.github.com>
2025-07-25 11:01:55 -0700

    TXWitness.cs

---
+++ BToken/Chaining/TXWitness.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BToken.Chaining
+{
+  partial class UTXOTable
+  {
+    class TXWitness
+    {
+      public static TXWitness Parse(byte[] byteStream, ref int startIndex)
+      {
+        return new TXWitness();
+      }
+    }
+  }
+}
